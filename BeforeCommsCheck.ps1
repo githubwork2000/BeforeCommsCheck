42b1c69975623ef2a981c8776040db49ba602159
githubwork2000 <126277717+githubwork2000@users.noreply.github.com>
2023-03-03 14:01:59 -0800

    Update BeforeCommsCheck.ps1
    formatting

--- BeforeCommsCheck.ps1
+++ BeforeCommsCheck.ps1
@@ -1,22 +1,24 @@
 $ErrorActionPreference = "silentlycontinue"
 
 Function Get-VCSReplication {
-   # Get all the VCS services on the local machine
+    # Get all the VCS services on the local machine
     $vcs = Get-WmiObject -Namespace Root\Citrix -Class Xen_VCSService
     # Loop through each of the services detected
     foreach ($vc in $vcs) {
         # Get the current state
         $state = $vc.GetState()
         # Check if its running
-        if ($state -eq 0){
+        if ($state -eq 0) {
             # Display the hostname and active state
             Write-Host "Hostname: $($vc.hostname) - Status: Active"
-        # Check if its replicating
-        } elseif ($state -eq 3) {
+            # Check if its replicating
+        }
+        elseif ($state -eq 3) {
             # Display the hostname and Replicating status
             Write-Host "Hostname: $($vc.hostname) - Status: Replicating"
-        # If it's neither running nor replicating then display as Not Running
-        } else {
+            # If it's neither running nor replicating then display as Not Running
+        }
+        else {
             Write-Host "Hostname: $($vc.hostname) -Status: Not Running"
         }
     }
@@ -60,7 +62,7 @@ Function Get-PublicIpAddress {
 }
 
 function Get-BackupIPAddress {
-    $backupIP = (Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object {$_.IPAddress -like "10.*"})[0].IPAddress
+    $backupIP = (Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object { $_.IPAddress -like "10.*" })[0].IPAddress
     
     if (!$backupIP) {
         Write-Output "No Backup IP Address found."
@@ -71,26 +73,26 @@ function Get-BackupIPAddress {
 }
 
 function Get-ReplicationIP {
-  $networkInterfaces = Get-NetIPConfiguration | Where-Object { $_.InterfaceAlias -notlike "*Loopback*" -and $_.InterfaceOperationalStatus -eq "Up" }
+    $networkInterfaces = Get-NetIPConfiguration | Where-Object { $_.InterfaceAlias -notlike "*Loopback*" -and $_.InterfaceOperationalStatus -eq "Up" }
   
-  # check each active network interface for replicationip
-  foreach ($interface in $networkInterfaces) {
-    $replicationIPs = Get-NetAdapterRsc | Where-Object { $_.Name -eq $interface.InterfaceAlias } -and Where-Object { $_.AddressState -eq 'Preferred' } | Select-Object -ExpandProperty IPv4Address
-    if ($replicationIPs) {
-      return $replicationIPs.IPAddressToString
+    # check each active network interface for replicationip
+    foreach ($interface in $networkInterfaces) {
+        $replicationIPs = Get-NetAdapterRsc | Where-Object { $_.Name -eq $interface.InterfaceAlias } -and Where-Object { $_.AddressState -eq 'Preferred' } | Select-Object -ExpandProperty IPv4Address
+        if ($replicationIPs) {
+            return $replicationIPs.IPAddressToString
+        }
     }
-  }
 
-  # if no replicationip found, return null or write error message
-  Write-Error "Could not find the replication IP address."
-  return $null
+    # if no replicationip found, return null or write error message
+    Write-Error "Could not find the replication IP address."
+    return $null
 }
 
 function Get-DNSNames {
     $dnsNames = @()
 
     # Get all IP addresses assigned to the server
-    $ips += (Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceAlias -notlike "*Loopback*"}) | Select IPAddress -ExpandProperty IPAddress
+    $ips += (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -notlike "*Loopback*" }) | Select IPAddress -ExpandProperty IPAddress
 
     # For each IP address, get the corresponding DNS name using nslookup
     foreach ($ip in $ips) {
@@ -114,12 +116,13 @@ function Main {
                                                                         
 '@
 
-Get-PublicIpAddress
-Get-BackupIPAddress
-Get-ReplicationIP
-Get-DNSNames
-Get-ServerServices
-Get-VCSReplication
+    Get-PublicIpAddress
+    Get-BackupIPAddress
+    Get-ReplicationIP
+    Get-DNSNames
+    Get-ServerServices
+    Get-DNSNames
+    Get-VCSReplication
 
 }
 
