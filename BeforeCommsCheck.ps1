945cfbebe018c54405c6363687d3e17783ccbfa5
githubwork2000 <126277717+githubwork2000@users.noreply.github.com>
2023-03-07 06:54:36 -0800

    Update BeforeCommsCheck.ps1
    better

--- BeforeCommsCheck.ps1
+++ BeforeCommsCheck.ps1
@@ -1,118 +1,138 @@
 #$cred = Get-Credential
 
-$ErrorActionPreference = "silentlycontinue"
+$scriptblock = {
 
-function Get-Hostname {
-    $hostname = $env:computername
-    return $hostname
-}
-function Test-VeritasPath {
-Test-Path -Path "C:\Program Files\VERITAS\"
-}
+    $ErrorActionPreference = "silentlycontinue"
 
-function Get-ServerServices {
-
-    [string]$servertypes = ""
-
-    $services = Get-Service
-
-    if ($services | Where-Object { $_.DisplayName -match "SQL Server" }) {
-        $servertypes = $servertypes + "MSSQL-"
+    function Get-Hostname {
+        $hostname = $env:computername
+        return $hostname
     }
 
-    if ($services | Where-Object { $_.DisplayName -match "IIS" }) {
-        $servertypes = $servertypes +  "IIS-"
+    function Test-VeritasPath {
+        Test-Path -Path "C:\Program Files\VERITAS\"
     }
+    function Get-ServerServices {
 
-    if ($services | Where-Object { $_.DisplayName -match "FTP" }) {
-        $servertypes = $servertypes +  "FTP-"
-    }
+        [string]$servertypes = ""
 
-    $defaultShares = @('ADMIN$', 'C$', 'IPC$')
-    if (Get-WmiObject -Class Win32_Share | Where-Object {$_.Type -eq 0 -and $defaultShares -notcontains $_.Name} | Select-Object Name, Path) {
-        $servertypes = $servertypes +  "File-"
-    }
+        $services = Get-Service
 
-    $printQueues = Get-WmiObject Win32_PrintQueue -ErrorAction SilentlyContinue
-    if ($printQueues.Count -gt 0) {
-        $servertypes = $servertypes +  "Print-"
-    }
-
-    if ((Get-Service  | Where-Object { $_.DisplayName -match "MQ" }) -or
-    (Get-Process -Name "mqsvc" -ErrorAction SilentlyContinue)) {
-        $servertypes = $servertypes +  "MSMQ-"
-    }
-
-    $servertypes = $servertypes.Substring(0, $servertypes.Length - 1)
-    
-    Return $servertypes
-    
-}
-
-Function Get-PublicIpAddress { 
-    $WebClient = New-Object System.Net.WebClient
-    $PublicIp = $WebClient.DownloadString("https://api.ipify.org")
-    return $PublicIp
-}
-
-Function Get-BackupIPAddress {
-$backupIP = (Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object { $_.IPAddress -like "10.*" })[0].IPAddress
-    return
-}
-
-Function Get-ReplicationIP {
-    $networkInterfaces = Get-NetIPConfiguration | Where-Object { $_.InterfaceAlias -notlike "*Loopback*" -and $_.InterfaceOperationalStatus -eq "Up" }
-  
-    # check each active network interface for replicationip
-    foreach ($interface in $networkInterfaces) {
-        $replicationIPs = Get-NetAdapterRsc | Where-Object { $_.Name -eq $interface.InterfaceAlias } -and Where-Object { $_.AddressState -eq 'Preferred' } | Select-Object -ExpandProperty IPv4Address
-        if ($replicationIPs) {
-            return $replicationIPs.IPAddressToString
+        if ($services | Where-Object { $_.DisplayName -match "SQL Server" }) {
+            $servertypes = $servertypes + "MSSQL-"
         }
+
+        if ($services | Where-Object { $_.DisplayName -match "IIS" }) {
+            $servertypes = $servertypes + "IIS-"
+        }
+
+        if ($services | Where-Object { $_.DisplayName -match "FTP" }) {
+            $servertypes = $servertypes + "FTP-"
+        }
+
+        $defaultShares = @('ADMIN$', 'C$', 'IPC$')
+        if (Get-WmiObject -Class Win32_Share | Where-Object { $_.Type -eq 0 -and $defaultShares -notcontains $_.Name } | Select-Object Name, Path) {
+            $servertypes = $servertypes + "File-"
+        }
+
+        $printQueues = Get-WmiObject Win32_PrintQueue -ErrorAction SilentlyContinue
+        if ($printQueues.Count -gt 0) {
+            $servertypes = $servertypes + "Print-"
+        }
+
+        if ((Get-Service  | Where-Object { $_.DisplayName -match "MQ" }) -or
+    (Get-Process -Name "mqsvc" -ErrorAction SilentlyContinue)) {
+            $servertypes = $servertypes + "MSMQ-"
+        }
+
+        $servertypes = $servertypes.Substring(0, $servertypes.Length - 1)
+    
+        Return $servertypes
+    
     }
 
-    # if no replicationip found, return null or write error message
-    #Write-Error "Could not find the replication IP address."
-    return $null
-}
-
-function Get-DNSNames {
-    $dnsNames = @()
-
-    # Get all IP addresses assigned to the server
-    $ips += (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -notlike "*Loopback*" }) | Select-Object IPAddress -ExpandProperty IPAddress
-
-    # For each IP address, get the corresponding DNS name using nslookup
-    foreach ($ip in $ips) {
-
-        [System.Net.Dns]::GetHostEntry($ip).HostName
+    Function Get-PublicIpAddress { 
+        $WebClient = New-Object System.Net.WebClient
+        $PublicIp = $WebClient.DownloadString("https://api.ipify.org")
+        return $PublicIp.IPAddressToString
     }
 
-    return $dnsNames
+    function Get-BackupIPAddress {
+        $backupIP = (Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object { $_.IPAddress -like "10.*" })[0].IPAddress
+    
+        if (!$backupIP) {
+            #Write-Output "No Backup IP Address found."
+            return $null
+        }
+    
+        return $backupIP
+    }
+
+    function Get-ReplicationIP {
+        $networkInterfaces = Get-NetIPConfiguration | Where-Object { $_.InterfaceAlias -notlike "*Loopback*" -and $_.InterfaceOperationalStatus -eq "Up" }
+  
+        # check each active network interface for replicationip
+        foreach ($interface in $networkInterfaces) {
+            $replicationIPs = Get-NetAdapterRsc | Where-Object { $_.Name -eq $interface.InterfaceAlias } -and Where-Object { $_.AddressState -eq 'Preferred' } | Select-Object -ExpandProperty IPv4Address
+            if ($replicationIPs) {
+                return $replicationIPs.IPAddressToString
+            }
+        }
+
+        # if no replicationip found, return null or write error message
+        #Write-Error "Could not find the replication IP address."
+        return $null
+    }
+
+    function Get-DNSNames {
+        $dnsNames = @()
+
+        # Get all IP addresses assigned to the server
+        $ips += (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -notlike "*Loopback*" }) | Select-Object IPAddress -ExpandProperty IPAddress
+
+        # For each IP address, get the corresponding DNS name using nslookup
+        foreach ($ip in $ips) {
+
+            [System.Net.Dns]::GetHostEntry($ip).HostName
+        }
+
+        return $dnsNames
+    }
+
+    function Main {
+
+        $hostname = Get-Hostname
+        $publicIP = Get-PublicIpAddress
+        $backupIP = Get-BackupIPAddress
+        $replicationIP = Get-ReplicationIP
+        $dnsName = Get-DNSNames
+        $services = Get-ServerServices
+        $veritas = Get-DNSNames
+
+        [PSCustomObject]@{
+            Hostname             = $hostname
+            PublicIPAddress      = $publicIP
+            BackupIPAddress      = $backupIP
+            ReplicationIPAddress = $replicationIP
+            DNSName              = $dnsName
+            ServerType           = $services
+            VCSVVR               = $veritas -join ', '
+        }
+
+        $gold = "{0},{1},{2},{3},{4},{5},{6}" -f $hostname, $publicIP, $backupIP, $replicationIP, $dnsName, $services, $veritas
+        return $gold
+
+        Get-DNSNames
+        Get-VCSReplication
+
+    }
+} #End of scriptblock
+
+$nodes = "localhost"
+foreach ($node in $nodes) {
+    Write-Host "`n"
+    Write-Host "Executing on node $node"
+    $gold = Invoke-Command -ComputerName "$node" -scriptblock $scriptblock -Credential $cred
+    $gold | add-content -path C:\output.csv
 }
-
-function Main {
-
-    $hostname = Get-Hostname
-    $publicIP = Get-PublicIpAddress
-    $backupIP = Get-BackupIPAddress
-    $replicationIP = Get-ReplicationIP
-    $dnsName = Get-DNSNames
-    $services = Get-ServerServices
-    $veritas = Test-VeritasPath
-
-    $gold = "{0},{1},{2},{3},{4},{5},{6}" $hostname,$publicIP,$backupIP,$replicationIP,$dnsName,$services,$veritas
-    return $gold  
-}
-
 Main
-
-
-#$nodes = "localhost"
-#    foreach($node in $nodes)
-#    {
-#        Write-Host "`n"
-#        Write-Host "Executing on node $node"
-#        $gold = Invoke-Command -ComputerName "$node" -scriptblock $scriptblock -Credential $cred
-#        $gold | add-content -path C:\output.csv
-#    }
